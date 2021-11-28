84370f2b72196c0d9f77d90fa7312b50253e93d4
genemane <eugene.septe@yandex.ru>
2021-11-29 01:24:57 +0700

    Добавил больше комментов

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import PySimpleGUI as sg
-layout_main_menu = [           #Начальный пресет
+layout_main_menu = [              #Начальный пресет
     [sg.Canvas(size=(300,100), key=('upper_canvas'))],
     [sg.Canvas(size=(90,1), key=('cen_left_canvas')), sg.Button(button_text=('Войти'), size=(10,1), key=('cen_button'))],
     [sg.Canvas(size=(300, 100), key=('down_canvas'))]
 ]
-layout_after_login = [         #Пресет после клика кнопки Войти
+layout_after_login = [            #Пресет после клика кнопки Войти
     [sg.Canvas(size=(300,100))],
     [sg.Canvas(size=(60,1)), sg.Text(text='Введите логин'),sg.InputText(size=(20,1))],
     [sg.Canvas(size=(300, 100))]
 ]
 window = sg.Window('PSA', layout_main_menu)
-while True:
+while True:						 #Основной цикл обновления окна и приём ивентов
     event, values = window.read()
     if event in (None, 'Далее', 'Закрыть'):
         break
