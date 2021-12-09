3d0122e916271561c2f4f34ed6bf555043a9b07f
vikiFOXx <vshepan12@gmail.com>
2021-12-09 18:30:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,21 +8,53 @@ layout_main_menu = [              #Начальный пресет
    [sg.Canvas(size=(150,5))],
    [sg.Canvas(size=(0,0)), sg.Text(text='Введите пароль'),sg.InputText(size=(20,1))],
    [sg.Canvas(size=(150,15))],
-   [sg.Canvas(size=(90,1), key=('cen_left_canvas')), sg.Button(button_text=('Войти'), size=(10,1), key=('enter'))],
+   [sg.Canvas(size=(90,1), key=('cen_left_canvas1')), sg.Button(button_text=('Войти'), size=(10,1), key=('enter'))],
    [sg.Canvas(size=(150,5))],
    [sg.Canvas(size=(75,0)), sg.Text(text='У Вас нет аккаунта?')],
-   [sg.Canvas(size=(62,1), key=('cen_left_canvas')), sg.Button(button_text=('Зарегистрироваться'), size=(17,1), key=('register'))],
+   [sg.Canvas(size=(62,1), key=('cen_left_canvas2')), sg.Button(button_text=('Зарегистрироваться'), size=(17,1), key=('register'))],
    [sg.Canvas(size=(280, 25))]
 ]
-
+que = [
+   '1. Мне требуется много времени, чтобы “раскачаться” и начать действовать',
+   '2. Я планирую мои дела ежедневно',
+   '3. Меня выводят из себя и выбивают из привычного графика непредвиденные дела',
+   '4. Обычно я намечаю программу на день и стараюсь ее выполнить',
+   '5. Мне бывает трудно завершить начатое',
+   '6. Я не могу отказаться от начатого дела, даже если оно мне «не по зубам»',
+   '7. Я знаю, чего хочу, и делаю все, чтобы этого добиться',
+   '8. Я заранее выстраиваю план предстоящего дня',
+   '9. Мне более важно то, что я делаю и переживаю в данный момент, а не то, что будет или был',
+   '10. Я могу начать делать несколько дел и ни одно из них не закончить',
+   '11. Я планирую мои повседневные дела согласно определенным принципам',
+   '12. Я считаю себя человеком, живущим «здесь и сейчас»',
+   '13. Я не могу перейти к другому делу, если не завершил предыдущего',
+   '14. Я считаю себя целенаправленным человеком',
+   '15. Вместо того чтобы заниматься делами, я часто попусту трачу время',
+   '16. Мне нравится вести дневник и фиксировать в нем происходящее со мной',
+   '17. Иногда я даже не могу заснуть, вспомнив о недоделанных делах',
+   '18. Мне есть к чему стремиться',
+   '19. Мне нравится пользоваться ежедневником и иными средствами планирования времени',
+   '20. Моя жизнь направлена на достижение определенных результатов',
+   '21. У меня бывают трудности с упорядочением моих дел',
+   '22. Мне нравится писать отчеты по итогам работы',
+   '23. Я ни к чему не стремлюсь',
+   '24. Если я не закончил какое-то дело, то это не выходит у меня из головы',
+   '25. У меня есть главная цель в жизни'
+]
+count=0
 layout_after_login = [            #Пресет после клика кнопки Войти
    [sg.Canvas(size=(1000,2))],
-   [sg.Canvas(size=(50,1)), sg.Text(text='Для начала работы Вам необходимо пройти опрос на определение Вашего уровня самоорганизации')],
-   [sg.Canvas(size=(1,10)), sg.Text(text='Вам предлагается ряд утверждений, касающихся \nразличных сторон Вашей жизни и способов обращения \nсо временем. Введите в поле ту цифру, которая в \nнаибольшей мере характеризует \nВас и отражает Вашу точку зрения (1 — полное несогласие, \n7 — полное согласие с данным утверждением, 4 — \nсередина шкалы, остальные цифры — промежуточные значения). ')],
-   [sg.Canvas(size=(2,10)), sg.Text(text='1. Мне требуется много времени, чтобы “раскачаться” и начать действовать'), sg.Input('0', size=(2,1),key = ('input1'))],
+   [sg.Canvas(size=(200,30)), sg.Text(text='Для начала работы Вам необходимо пройти опрос на определение Вашего уровня самоорганизации')],
+   [sg.Canvas(size=(1,1)), sg.Text(text='Вам предлагается 25 утверждений, касающихся различных сторон Вашей жизни и способов обращения со временем. Введите в поле ту цифру, которая ')],
+   [sg.Canvas(size=(1,1)), sg.Text(text='в наибольшей мере характеризует Вас и отражает Вашу точку зрения (1 — полное несогласие, 7 — полное согласие с данным утверждением, 4 — середина ')],
+   [sg.Canvas(size=(1,1)), sg.Text(text='шкалы, остальные цифры — промежуточные значения)')],
+   [sg.Canvas(size=(2,10)), sg.Text(text=que[count], key=('opros')), sg.Input('', size=(2,1),key = ('input1'))],
+   [sg.Canvas(size=(90,1), key=('cen_left_canvas3')), sg.Button(button_text=('Далее'), size=(10,1), key=('next'))],
+   [sg.Canvas(size=(2,10)), sg.Input('', size=(3,1),key = ('vvv'))],
    [sg.Canvas(size=(1000, 5))]
 ]
 window = sg.Window('Органайзер', layout_main_menu)
+sum=0
 while True:
    event, values = window.Read()
    if event in (None, sg.WIN_CLOSED):
@@ -30,6 +62,57 @@ while True:
    elif 'enter' in event:
       window.close()
       window = sg.Window('Опрос', layout_after_login)
+   elif 'next' in event:
+      if count == 0 or count == 4 or count == 9 or count == 14 or count == 20 or count == 22:
+         if int(values['input1']) == 1:          #Подозреваю, что эта конструкция мега неоптимизированная, но поменяете если надо,
+            sum=sum+7                            #я пока не знаю как, не нашла тут switch - case
+            window.Element('vvv').Update(value=sum)
+            count=count+1
+            window.Element('opros').Update(value=que[count])
+            window.Element('input1').Update(value='')
+         if int(values['input1']) == 7:
+            sum=sum+1
+            window.Element('vvv').Update(value=sum)
+            count=count+1
+            window.Element('opros').Update(value=que[count])
+            window.Element('input1').Update(value='')
+         if int(values['input1']) == 2:
+            sum=sum+6
+            window.Element('vvv').Update(value=sum)
+            count=count+1
+            window.Element('opros').Update(value=que[count])
+            window.Element('input1').Update(value='')
+         if int(values['input1']) == 6:
+            sum=sum+2
+            window.Element('vvv').Update(value=sum)
+            count=count+1
+            window.Element('opros').Update(value=que[count])
+            window.Element('input1').Update(value='')
+         if int(values['input1']) == 3:
+            sum=sum+5
+            window.Element('vvv').Update(value=sum)
+            count=count+1
+            window.Element('opros').Update(value=que[count])
+            window.Element('input1').Update(value='')
+         if int(values['input1']) == 5:
+            sum=sum+3
+            window.Element('vvv').Update(value=sum)
+            count=count+1
+            window.Element('opros').Update(value=que[count])
+            window.Element('input1').Update(value='')
+         if int(values['input1']) == 4:
+            sum=sum+4
+            window.Element('vvv').Update(value=sum)
+            count=count+1
+            window.Element('opros').Update(value=que[count])
+            window.Element('input1').Update(value='')
+      else:
+         sum=sum+int(values['input1'])
+         window.Element('vvv').Update(value=sum)                #Пыталась сделать через условие, чтоб после 25 вопроса либо обнулялся счётчик, либо кнопка становилась невидимой
+         count=count+1                                          #Но пока не получилось, всё равно вылетает, поэтому я удалила свои неудачные попытки
+         window.Element('opros').Update(value=que[count])
+         window.Element('input1').Update(value='')
+         #window.Element('next').Update(visible=False)
 
 
 
